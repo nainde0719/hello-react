33ac9bf7e5498ae2e724675edfb4b2ffb51393ea
nainde0719 <nainde0719@gmail.com>
2020-05-20 23:30:55 +0900

    2.4.3 if문 대신 조건부 연산자

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import React, { Component } from 'react';
 class App extends Component {
     render() {
         const text = '당신은 어썸한가요?';
+        const condition = true;
+        
         return (
             <div>
                 <h1>리액트 안녕!</h1>
                 <h2>{text}</h2>
+                {
+                    condition ? '참' : '거짓'
+                }
             </div>
         );
     }
