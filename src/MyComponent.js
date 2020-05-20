e1a9f7f2818ccb27e6e284eb51c8c1bdc176476b
nainde0719 <nainde0719@gmail.com>
2020-05-21 00:18:21 +0900

    작업중

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -12,11 +12,19 @@ class MyComponent extends Component {
         age: PropTypes.number.isRequired // 필수적으로 존재하며, 숫자이다.
     }
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            number:0
+        }
+    }
+
     render() {
         return (
             <div>
                 <p>안녕하세요 제 이름은 {this.props.name} 입니다.</p>
                 <p>저는 {this.props.age}살 입니다.</p>
+                <p>숫자: {this.state.number}</p>
             </div>
         )
     }
