bf1cf5b2ee76f325d18714406ab3db0d0c305ada
nainde0719 <nainde0719@gmail.com>
2020-05-20 23:31:57 +0900

    2.4.5 인라인 스타일링(camelCase)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    render() {
-        const text = '당신은 어썸한가요?';
-        const condition = true;
-        
-        return (
-            <div>
-                <h1>리액트 안녕!</h1>
-                <h2>{text}</h2>
-                {
-                    condition ? '참' : '거짓'
-                }
-            </div>
-        );
-    }
+  render() {
+      const text = '당신은 어썸한가요?';
+      const condition = true;
+      const style = {
+        backgroundColor: 'gray',
+        border: '1px solid black',
+        height: Math.round(Math.random() * 300) + 50,
+        width: Math.round(Math.random() * 300) + 50,
+        webkitTransition: 'all',
+        MozTransition: 'all',
+        msTransition: 'all'
+      };
+      
+      return (
+          <div>
+              <h1>리액트 안녕!</h1>
+              <h2>{text}</h2>
+              { condition && '보여주세요' }
+              <div style={style}></div>
+          </div>
+      );
+  }
 }
 
 export default App;
