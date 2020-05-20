f31d424a16e46f11946da438b5c32ce339073e70
nainde0719 <nainde0719@gmail.com>
2020-05-20 23:12:48 +0900

    2.4.1 감싸인 요소 _error

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <div className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h2>Welcome to React</h2>
-                </div>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
-            </div>
+            <h1>리액트 안녕!</h1>
+            <h2>당신은 어썸한가요?</h2>
         );
     }
 }
+
+export default App;
