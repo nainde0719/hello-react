b3fb6dd80923a80eae255977e2f0d2fa6cc894cc
nainde0719 <nainde0719@gmail.com>
2020-05-20 23:50:37 +0900

    3.1.3.2 모듈 불러오기(import)

--- src/App.js
+++ src/App.js
@@ -1,29 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import MyComponent from './MyComponent'; // MyComponent 파일을 불러옵니다.
 
 class App extends Component {
-  render() {
-      const text = '당신은 어썸한가요?';
-      const condition = true;
-      const style = {
-        backgroundColor: 'gray',
-        border: '1px solid black',
-        height: Math.round(Math.random() * 300) + 50,
-        width: Math.round(Math.random() * 300) + 50,
-        webkitTransition: 'all',
-        MozTransition: 'all',
-        msTransition: 'all'
-      };
-      
-      return (
-          <div  className/* not class */="my-div">
-              <h1>리액트 안녕!</h1>
-              <h2>{text}</h2>
-              { condition && '보여주세요' }
-              <div style={style}></div>
-          </div>
-      );
-  }
+    render() {
+        return (
+            <MyComponent/>
+        )
+    }
 }
 
-export default App;
+export default MyComponent;
