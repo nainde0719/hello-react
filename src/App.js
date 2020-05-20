1c22424d2132d4a86c3be637d3e317c428d96ee5
nainde0719 <nainde0719@gmail.com>
2020-05-20 23:23:16 +0900

    App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React, { Component, Fragment } from 'react';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-    render() {
-        return (
-            <Fragment>
-                <h1>리액트 안녕!</h1>
-                <h2>당신은 어썸한가요?</h2>
-            </Fragment>
-        );
-    }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
