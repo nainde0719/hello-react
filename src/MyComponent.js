c115af21eaef7513e8b11e79d995acf7d14b2179
nainde0719 <nainde0719@gmail.com>
2020-05-21 00:07:44 +0900

    3.2.4 props 검증: propTypes_class 밖

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
     static defaultProps = {
@@ -11,6 +12,10 @@ class MyComponent extends Component {
             </div>
         )
     }
+}
+
+MyComponent.propTypes = {
+    name: PropTypes.string // name props 타입을 문자열로 설정
 }
 
 export default MyComponent;
