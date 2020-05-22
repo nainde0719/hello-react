70fd964daf10421b9f5c979ba8a616fef2d40f69
nainde0719 <nainde0719@gmail.com>
2020-05-22 21:26:30 +0900

    3.3.1 컴포넌트의 생성자 메서드: comstructor()

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -2,29 +2,24 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class MyComponent extends Component {
-    
     static defaultProps = {
         name: '기본 이름'
     }
 
     static propTypes = {
         name: PropTypes.string, // name props 타입을 문자열로 설정
-        age: PropTypes.number.isRequired // 필수적으로 존재하며, 숫자이다.
+        age: PropTypes.number.isRequired // 필수적으로 존재해야 하며, 숫자
     }
 
     constructor(props) {
         super(props);
-        this.state = {
-            number:0
-        }
     }
 
     render() {
         return (
             <div>
-                <p>안녕하세요 제 이름은 {this.props.name} 입니다.</p>
-                <p>저는 {this.props.age}살 입니다.</p>
-                <p>숫자: {this.state.number}</p>
+            <p>안녕하세요, 제 이름은 {this.props.name} 입니다.</p>
+            <p>저는 {this.props.age}살 입니다.</p>
             </div>
         )
     }
