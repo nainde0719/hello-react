30e39ce692858546fab30b9a6261d51d0755007f
nainde0719 <nainde0719@gmail.com>
2020-05-27 22:39:59 +0900

    5.2.2.2 버튼 onClick 이벤트 코드 수정

--- src/ValidationSample.js
+++ src/ValidationSample.js
@@ -18,7 +18,8 @@ handleButtonClick = () => {
     this.setState({
         clicked: true,
         validated: this.state.password === '0000'
-    })
+    });
+    this.input.focus();
 }
 
 render() {
