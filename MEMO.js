a725386cb58d6abe7847360dd4c0556438ca920c
nainde0719 <nainde0719@gmail.com>
2020-05-27 22:44:31 +0900

    5.3 컴포넌트에 ref 달기     5.3.1 사용법

--- MEMO.js
+++ MEMO.js
@@ -1,2 +1,8 @@
 <div id="my-element"></div>
 <input ref={(ref) => {this.input=ref}}></input>
+<input ref={(ref) => {this.input=ref}}
+/>
+
+<MyComponent
+    ref={(ref) => {this.myComponent=ref}}
+    />
