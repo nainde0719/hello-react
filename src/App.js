773747c051e42197e1eb6c65a930b8e48a79a96b
nainde0719 <nainde0719@gmail.com>
2020-05-27 22:50:41 +0900

    5.3.2 컴포넌트 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import EventPractice from './ValidationSample';
-import ValidationSample from './ValidationSample';
+import ScrollBox from './ScrollBox';
 
 class App extends Component {
     render() {
         return (
-            <ValidationSample/>
+            <ScrollBox/>
         );
     }
 }
