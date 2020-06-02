affab579cf0a72ee185439dffbe202904edd702e
nainde0719 <nainde0719@gmail.com>
2020-06-02 20:12:46 +0900

    6.1_2

--- MEMO.js
+++ MEMO.js
@@ -1,6 +1,3 @@
-var numbers = [1, 2, 3, 4, 5];
-var processed = numbers.map(function (num) {
-    return num * num;
-});
-
-console.log(processed);
+const numbers = [1, 2, 3, 4, 5];
+const result = numbers.map(num => num * num);
+console.log(result);
